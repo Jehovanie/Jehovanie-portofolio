893cd72c41c4d00a6c892f94d605ccdda61e7be4
Jehovanie <jehovanieram@gmail.com>
2022-04-15 19:35:19 +0300

    Setting nav bar css

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <section id="section">
+        <section id="about">
             about
         </section>
     );
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,15 +4,45 @@ import { AiOutlineUser } from 'react-icons/ai';
 import { RiServiceLine } from 'react-icons/ri';
 import { BiBook } from 'react-icons/bi';
 import { BiMessageSquareDetail } from 'react-icons/bi';
+import './nav.css';
 
 const Nav = () => {
+
+    const [activeNav, setActiveNav] = React.useState("#");
+
+
     return (
         <nav>
-            <a href="#"><AiOutlineHome /></a>
-            <a href="#about"><AiOutlineUser /></a>
-            <a href="#experience"><BiBook /></a>
-            <a href="#services"><RiServiceLine /></a>
-            <a href="#contact"><BiMessageSquareDetail /></a>
+            <a href="#"
+                onClick={() => setActiveNav("#")}
+                className={activeNav === "#" ? 'active' : ""}
+            >
+                <AiOutlineHome />
+            </a>
+            <a href="#about"
+                onClick={() => setActiveNav("#about")}
+                className={activeNav === "#about" ? 'active' : ""}
+            >
+                <AiOutlineUser />
+            </a>
+            <a href="#experience"
+                onClick={() => setActiveNav("#experience")}
+                className={activeNav === "#experience" ? 'active' : ""}
+            >
+                <BiBook />
+            </a>
+            <a href="#services"
+                onClick={() => setActiveNav("#services")}
+                className={activeNav === "#services" ? 'active' : ""}
+            >
+                <RiServiceLine />
+            </a>
+            <a href="#contact"
+                onClick={() => setActiveNav("#contact")}
+                className={activeNav === "#contact" ? 'active' : ""}
+            >
+                <BiMessageSquareDetail />
+            </a>
         </nav>
     );
 };
