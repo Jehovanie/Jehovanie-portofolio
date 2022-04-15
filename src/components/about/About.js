1e63d9e081fcd25aca914ee3bb610a2456d36197
Jehovanie <jehovanieram@gmail.com>
2022-04-15 20:41:51 +0300

    make the style of the about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import ME from '../../assets/image-1.png';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
+import './about.css';
 
 const About = () => {
     return (
         <section id="about">
-            about
+            <h5> Get to Know</h5>
+            <h2> About Me</h2>
+
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="About image" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <FaAward class="about__icon" />
+                            <h5> Experience </h5>
+                            <small> 3+ Years Working </small>
+                        </article>
+                        <article className="about__card">
+                            <FiUsers class="about__icon" />
+                            <h5> Clients </h5>
+                            <small>200+ Worldwide </small>
+                        </article>
+                        <article className="about__card">
+                            <VscFolderLibrary class="about__icon" />
+                            <h5> Projects </h5>
+                            <small> 80+ Completed </small>
+                        </article>
+
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Corporis consectetur recusandae nesciunt ab, architecto obc
+                        aecati illo, accusamus unde nisi facere fuga voluptates magni, ea autem impedit minima placeat odit. Officia.
+                    </p>
+
+                    <a href="#contact" className='btn btn-primary'>
+                        Let's Talk
+                    </a>
+
+                </div>
+            </div>
         </section>
     );
 };
