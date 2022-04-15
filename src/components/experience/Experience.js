689b9c132e7c41f89de8f64ce28b91611f159c0d
Jehovanie <jehovanieram@gmail.com>
2022-04-15 21:28:27 +0300

    Setting up experience component and make their style

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,9 +1,112 @@
 import React from 'react';
+import { BsPatchCheckFill } from 'react-icons/bs';
+import "./experience.css";
 
 const Experience = () => {
     return (
         <section id="experience">
-            experience
+            <h5> What Skills I have </h5>
+            <h2> My Experience </h2>
+
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+
+                    </div>
+                </div>
+
+                <div className="experience__frontend">
+                    <h3>Backend  Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon " />
+                            <div>
+                                <h4> HTML</h4>
+                                <small className='text-light'> Intermediaire...</small>
+                            </div>
+                        </article>
+
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
